28d0b2bb884adcf98927a0fa0afc7e9f53f11855
lbandin <lucasblandin@hotmail.fr>
2023-09-17 20:27:44 +0200

    Connexion with the sql database

---
+++ NBA_project/NBA_project/Common/Models/Teams.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NBA_project.Common.Models
+{
+    public class Teams
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+
+        [Required]
+        public string? City { get; set; }
+
+        public virtual Players? Players { get; set; }
+        [ForeignKey(nameof(Players))]
+        public int PlayersId { get; set; }
+
+        public virtual Stades? Stades { get; set; }
+        [ForeignKey(nameof(Stades))]
+        public int StadesId { get; set; }
+    }
+}
